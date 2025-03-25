2e5b051a1b5c307de301e719cade17186d224310
Leandre GIAMMONA <leandre.giammona@gmail.com>
2025-03-25 15:06:11 +0100

    test_connexion_11

--- imposteur_aws/src/socket.js
+++ imposteur_aws/src/socket.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 console.log("URL du backend dans socket.js :", process.env.REACT_APP_BACKEND_URL);
 
-const socket = io(process.env.REACT_APP_BACKEND_URL, {
+const socket = io(process.env.REACT_APP_BACKEND_URL || 'https://imposteur-aws.onrender.com', {
     transports: ['websocket', 'polling'],
     withCredentials: true,
 });
