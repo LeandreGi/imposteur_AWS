7128f6e73da7c2f1f972b1175982b7abe883c7e9
Leandre GIAMMONA <leandre.giammona@gmail.com>
2025-03-25 15:27:46 +0100

    test_connexion_12

--- imposteur_aws/src/pages/socket.js
+++ imposteur_aws/src/pages/socket.js
@@ -1,4 +1,10 @@
-import io from 'socket.io-client';
+import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:4000');
+console.log("URL du backend dans socket.js :", process.env.REACT_APP_BACKEND_URL);
+
+const socket = io(process.env.REACT_APP_BACKEND_URL || 'https://imposteur-aws.onrender.com', {
+    transports: ['websocket', 'polling'],
+    withCredentials: true,
+});
+
 export default socket;
