92389a7b080e49c0cb2e0a326a80aaf8694f781c
Leandre GIAMMONA <leandre.giammona@gmail.com>
2025-03-25 14:11:35 +0100

    test_connexion_9

--- imposteur_aws/server.js
+++ imposteur_aws/server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: ['https://imposteur-aws.vercel.app', 'https://imposteur-aws.onrender.com'],
+        origin: '*',
         methods: ['GET', 'POST'],
         allowedHeaders: ["my-custom-header"],
         credentials: true,
--- imposteur_aws/src/socket.js
+++ imposteur_aws/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 const socket = io('https://imposteur-aws.onrender.com', {
-    transports: ['websocket'],
+    transports: ['websocket', 'polling'],
     withCredentials: true,
 });
 
